0583c60257ab795d84cf772f89fe9de67a80b1c6
Vasil1960 <v.tsigov@gmail.com>
2021-09-23 07:43:07 +0300

    DetailCard to controller

--- app/controller/VTController.js
+++ app/controller/VTController.js
@@ -22,9 +22,9 @@ Ext.define('VT.controller.VTController', {
             '#aboutBtn': {
                 tap: 'onAboutBtnTap'
             },
-            // '#vasil':{
-            //     leafitemtap: 'leafItemTap'
-            // }
+            'iagnestedlist':{
+                leafitemtap: 'leafItemTap'
+            }
         })
     },
 
@@ -58,9 +58,37 @@ Ext.define('VT.controller.VTController', {
     },
 
 
-    // leafItemTap: function(){
-    //     console.log('ttttt');
-    // }
+    leafItemTap: function(nestedList, list, index, target, record) {
+        console.log('leafitemtap');
+        var detailCard = nestedList.getDetailCard();
+        detailCard.setHtml([
+
+            '<table style="text-align:center">',
+            '    <tr>',
+            '        <td style="padding:5px"><img src="https://vasil.iag.bg/upload/'+record.get('glav_pod')+'/'+record.get('picture')+'" width="180" heigh="180">',
+            '    </td>',
+            '    <tr>',
+            '        <td><h1>'+record.get('text')+'</h1></td>',
+            '    </tr>',
+            '    <tr>',
+            '        <td><small>Поделение: </small><b>'+record.get('dgs')+'</td>',
+            '    </tr>',
+            '    <tr>',
+            '        <td><small>Моб. тел: </small><b>'+record.get('gsm')+'</b></td>',
+            '    </tr>',
+            '    <tr>',
+            '        <td><small>Email: </small><b>'+record.get('email')+'</b></td>',
+            '    </tr>',
+            '    <tr>',
+            '        <td><small>Длъжност: </small><b>'+record.get('dlagnost')+'</b></td>',
+            '    </tr>',
+            '    <tr>',
+            '        <td><small>Дирекция: </small><b>'+record.get('directorate')+'</b></td>',
+            '    </tr>  ',
+            // '   ',
+            '</table>'
+        ].join(""));
+    }
 
 });
 
