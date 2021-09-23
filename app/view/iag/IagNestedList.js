ab8e68cf25ef91835f01250cae2e1d6e8042f8e9
Vasil1960 <v.tsigov@gmail.com>
2021-09-23 07:40:00 +0300

    commit DetailCard

--- app/view/iag/IagNestedList.js
+++ app/view/iag/IagNestedList.js
@@ -16,6 +16,7 @@ Ext.define('VT.view.iag.IagNestedList',{
     },
 
     displayField: 'text',
+    
     toolbar: {
         items: [
             {
@@ -45,40 +46,7 @@ Ext.define('VT.view.iag.IagNestedList',{
                 type: 'json',
                 rootProperty: 'items',
             }
-        },
-    },
-
-    listeners: {
-        leafitemtap: function(nestedList, list, index, target, record) {
-            console.log('leafitemtap');
-            var detailCard = nestedList.getDetailCard();
-            detailCard.setHtml([
-
-                '<table style="text-align:center">',
-                '    <tr>',
-                '        <td style="padding:5px"><img src="https://vasil.iag.bg/upload/'+record.get('glav_pod')+'/'+record.get('picture')+'" width="180" heigh="180">',
-                '    </td>',
-                '    <tr>',
-                '        <td><h1>'+record.get('text')+'</h1></td>',
-                '    </tr>',
-                '    <tr>',
-                '        <td><small>Поделение: </small><b>'+record.get('dgs')+'</td>',
-                '    </tr>',
-                '    <tr>',
-                '        <td><small>Моб. тел: </small><b>'+record.get('gsm')+'</b></td>',
-                '    </tr>',
-                '    <tr>',
-                '        <td><small>Email: </small><b>'+record.get('email')+'</b></td>',
-                '    </tr>',
-                '    <tr>',
-                '        <td><small>Длъжност: </small><b>'+record.get('dlagnost')+'</b></td>',
-                '    </tr>',
-                '    <tr>',
-                '        <td><small>Дирекция: </small><b>'+record.get('directorate')+'</b></td>',
-                '    </tr>  ',
-                // '   ',
-                '</table>'
-            ].join(""));
         }
-    },
+    }
+
 });
